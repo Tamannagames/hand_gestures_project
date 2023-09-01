0a3e440e9180ba42d20b85ebd2187d982e0ca21e
Tamannagames <95958609+Tamannagames@users.noreply.github.com>
2023-08-31 23:12:17 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,28 +1,32 @@
 Webcam.set({
-    width: 350,
-    height: 300,
-    image_format: 'jpeg',
-    jpeg_quality: 90
-  });
-  
-  Webcam.attach('#camera');
-  
-  function captureImage() {
-    Webcam.snap(function(data_uri) {
-      document.getElementById('captured-image').innerHTML = '<img src="' + data_uri + '"/>';
-    });
-  }
-  
-  function identifyGesture() {
+  width: 350,
+  height: 300,
+  image_format: 'png',
+  png_quality: 90
+});
 
-    Webcam.snap(function(data_uri) {
-        document.getElementById("captured-image").innerHTML = '<img id="capture" src="'+data_uri+'"/>';
-    });
-  }
-  
-  console.log('ml5.js version:', ml5.version);
-  
-  const model = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/Jh_6XtBpC/model.json', function() {
-    console.log('Model loaded');
+var camera = document.getElementById("camera");
+Webcam.attach('#camera');
+
+function take_snapshot() {
+  Webcam.snap(function(data_uri) {
+    document.getElementById("view").innerHTML = '<img id="capture" src="' + data_uri + '"/>';
   });
-  
+}
+
+console.log('ml5 version', ml5.version);
+var classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/I7TyhUkno/model.json', modelLoaded);
+
+function modelLoaded() {
+  console.log('Model Loaded');
+}
+
+function speak() {
+  var synth = window.speechSynthesis;
+  var speak_data_1 = "The first prediction is " + prediction1;
+  var speak_data_2 = "The second prediction is " + prediction2;
+  var utterThis = new SpeechSynthesisUtterance(speak_data_1 + speak_data_2);
+  synth.speak(utterThis);
+}
+
+
